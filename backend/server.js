6189ed5a3ad23f9d914dda4e04b6671bea481e42
Eozef <www.github.com/Eozef.com>
2022-07-06 22:50:52 +1000

    Express App Setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+envoke;
+require("dotenv").config();
+
+const express = require("express");
+
+//express app
+const app = express();
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ mssg: "welcome to the app" });
+});
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("listeneing on port ", process.env.PORT);
+});
