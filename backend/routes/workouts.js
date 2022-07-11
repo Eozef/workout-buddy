46fda12c98584a60849ee7eca316d9d543046498
Eozef <www.github.com/Eozef.com>
2022-07-11 22:10:52 +1000

    Adding Models and Schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 
@@ -12,8 +13,15 @@ router.get("/:id", (req, res) => {
 });
 
 //POST a new workout
-router.post("/", (req, res) => {
-  res.json({ mssg: "POST a new workout" });
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
 });
 
 //DELETE a new workout
