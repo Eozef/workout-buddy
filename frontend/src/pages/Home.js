b4b3043ee5bec2ceb7a8b86e7f6b37a2f3548d53
eozefow <eozefow@gmail.com>
2022-07-16 23:05:02 +1000

    add proxy to get away from the CORS block, Fetching Data using, so workouts get to putput on the home page.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkdouts = async () => {
+      const res = await fetch("/api/workouts");
+
+      //pass in workout obj
+      const json = await res.json();
+
+      if (res.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkdouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
     </div>
   );
 };
