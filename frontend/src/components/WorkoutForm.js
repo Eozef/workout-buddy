be219ed239193a29d872c8b198c0f6a5e9ce405b
eozefow <eozefow@gmail.com>
2022-07-19 01:38:46 +1000

    Add New Workout Form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+  const [err, setErr] = useState(null);
+
+  const handleSubmit = async (e) => {
+    //to prevent the submition of the form
+    e.preventDefault();
+
+    const workout = { title, load, reps };
+
+    const res = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await res.json();
+
+    if (!res.ok) {
+      setErr(json.err);
+    }
+    if (res.ok) {
+      //erase the workout
+      setTitle("");
+      setLoad("");
+      setReps("");
+      setErr(null);
+      //output the new workout
+      console.log("new workout added", json);
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a new workout</h3>
+      <lable>Exercice Title:</lable>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+
+      <lable>Exercice Load (KG):</lable>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+
+      <lable>Exercice Reps:</lable>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+
+      <button>Submit</button>
+      {err && <div className="err">{err}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -27,6 +28,7 @@ const Home = () => {
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
