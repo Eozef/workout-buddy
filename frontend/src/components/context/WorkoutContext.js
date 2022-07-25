d42064217c01b9a594716b55524e197bea9aaaf1
eozefow <eozefow@gmail.com>
2022-07-26 02:04:44 +1000

    add WorkoutsContextProvider and workoutsReducer for managing states

---
+++ frontend/src/components/context/WorkoutContext.js
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from "react";
+
+export const WorkoutContext = createContext();
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    default:
+      return state;
+  }
+};
+// whatever components will need to access the Context it represents as the children prop is the root componet
+export const WorkoutsContextProvider = ({ children }) => {
+  // will update using once the data got passed in from dispatch()
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workout: null,
+  });
+
+  return (
+    <WorkoutContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WorkoutContext.Provider>
+  );
+};
