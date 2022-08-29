912ee56b8b5d4584d60e09410b7b401656d0891a
eozefow <eozefow@gmail.com>
2022-08-29 22:33:26 +1000

    JWT signin token is created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const { response } = require("express");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //login user
 const loginUser = async (req, res) => {
@@ -12,7 +17,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json({ email, user });
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
