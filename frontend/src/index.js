4c83b2c4e286ee21482eaf8333cf41c53ddc2e6a
eozefow <eozefow@gmail.com>
2022-08-31 01:29:42 +1000

    solve merge conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { WorkoutsContextProvider } from "./components/context/WorkoutContext";
+import { WorkoutsContextProvider } from "./context/WorkoutContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
